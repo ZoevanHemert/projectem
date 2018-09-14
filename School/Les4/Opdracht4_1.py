b3dae67747fce839af4a056945de83be7af5bb6a
ZoevanHemert <zoe.vanhemert@student.hu.nl>
2018-09-14 14:02:38 +0200

    done

---
+++ School/Les4/Opdracht4_1.py
@@ -0,0 +1,7 @@
+cijferICOR = 8
+cijferPROG = 7
+cijferCSN = 7
+gemiddelde = (cijferCSN + cijferICOR + cijferPROG) / 3
+beloning = (cijferCSN + cijferICOR + cijferPROG) * 30
+uitkomst = 'Mijn cijfers '+ str(gemiddelde) +' leveren een beloning van ' + str(beloning) + ' op!'
+print(uitkomst)
